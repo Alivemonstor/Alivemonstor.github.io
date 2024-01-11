6cb2d97e403146fb1f3de5b74206f233f5288868
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-01-11 16:00:02 +0000

    update yesss

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ $(document).ready(function(){
 })
 
 
+
+
+let slideIndex = 0;
+$(document).ready(function(){
+	showSlides();
+})
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} // if slideindex is greater than slides.length then slideindex = 1
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); 
+}

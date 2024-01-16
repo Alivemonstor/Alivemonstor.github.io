0de30aed00fce2afc77e95bfe1b8d4c49c4be83b
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-01-16 15:13:26 +0000

    cool new home page

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+let dvdclass = document.getElementsByClassName('infosection');
+let interval_id;
+let x_incr = 1;
+let y_incr = 1;
+let slideIndex = 0;
+
 $(document).ready(function(){
+	// showSlides();
+	setInterval(frameBounce, 5);
+
 	var i = 0;
 	var txt = 'Hi, I\'m Jayden, Welcome To My Website';
 	var speed = 70;
@@ -14,12 +23,33 @@ $(document).ready(function(){
 })
 
 
+function handle_collision() {
+  	let dvd_height = dvdclass.infoblock.offsetHeight;
+  	let dvd_width = dvdclass.infoblock.offsetWidth;
+  	let left = dvdclass.infoblock.offsetLeft;
+  	let top = dvdclass.infoblock.offsetTop;
+  	let win_height = document.getElementsByClassName('background-info').backgroundinfo.offsetHeight;
+  	let win_width = document.getElementsByClassName('background-info').backgroundinfo.offsetWidth;
+  	console.log(dvd_height, dvd_width, left, top, win_height,win_width)
+	
+  	if (left <= 0 || left + dvd_width >= win_width) {
+  	  x_incr = ~x_incr + 1;
+  	}
+  	if (top <= 0 || top + dvd_height >= win_height) {
+  	  y_incr = ~y_incr + 1;
+  	}
+}
 
+function frameBounce() {
+	let dvd_height = dvdclass.infoblock.offsetHeight;
+  	let dvd_width = dvdclass.infoblock.offsetWidth;
+  	let left = dvdclass.infoblock.offsetLeft;
+  	let top = dvdclass.infoblock.offsetTop;
+  	handle_collision();
+  	dvdclass.infoblock.style.top = top + y_incr;
+  	dvdclass.infoblock.style.left = left + x_incr;
+}
 
-let slideIndex = 0;
-$(document).ready(function(){
-	showSlides();
-})
 
 function showSlides() {
   let i;
@@ -32,3 +62,4 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000); 
 }
+

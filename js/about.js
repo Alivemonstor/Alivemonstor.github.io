bc36a3432d0404472e1af050e1f0cc851037090f
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2024-01-26 10:13:36 +0000

    pushing

--- js/about.js
+++ js/about.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-
+    if (!checkVisible($('.topnav'))) {
+		$('.movingnav').css("display", "block")
+		$('.topnav').css("display", "none")
+	} else {
+		$('.topnav').css("display", "block")
+		$('.movingnav').css("display", "none")
+	}
 })
 
 
+$(window).on( "scroll", function() {
+	if (!checkVisible($('.topnav'))) {
+		$('.movingnav').css("display", "block")
+		$('.movingnav').css("animation-name", "fadein")
+		$('.movingnav').css("animation-duration", "0.5s")
+		$('.topnav').css("display", "none")
+	} else {
+		$('.topnav').css("display", "block")
+		$('.topnav').css("animation-name", "fadein")
+		$('.topnav').css("animation-duration", "0.5s")
+		$('.movingnav').css("display", "none")
+	}
+});
